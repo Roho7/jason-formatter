f6d751eed9503a885b66320bdb776ee9ac6594eb
Roho7 <rohosen2@gmail.com>
2025-11-21 17:43:40 +0530

    add [og_image]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import { Figtree, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -15,8 +14,23 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "JASON Formatter",
-  description: "JASON DERULO",
+  title: "JSON Diff, Format and Convert",
+  description: "A free tool to diff, prettify, convert and validate JSON",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "JSON Diff, Format, Convert and Validate",
+    description: "A free tool to diff, prettify, convert and validate JSON",
+    images: [
+      {
+        url: "https://jason-formatter.vercel.app/og_image.png",
+        width: 1200,
+        height: 477,
+        alt: "JSON Diff, Format, Convert and Validate",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -29,7 +43,10 @@ export default function RootLayout({
       <head></head>
       <body className={`${figTree.variable} ${geistMono.variable} antialiased`}>
         {children}
-        <Script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js" strategy="beforeInteractive"></Script>
+        <Script
+          src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
+          strategy="beforeInteractive"
+        ></Script>
         <Script id="ko-fi-widget">
           {`kofiWidgetOverlay.draw('roho', {
     'type': 'floating-chat',
